5ecba9e550b1f1d2023cf6df4fe62e54dd068d56
Foez Ahmed <foez.official@gmail.com>
2025-12-26 15:54:17 +0600

    modified:   testbench/vco_tb.sv new file:   vco_tb_issue.gtkw

--- testbench/vco_tb.sv
+++ testbench/vco_tb.sv
@@ -16,24 +16,52 @@ module vco_tb;
       .clk_o(clk_o)
   );
 
+  realtime this_cycle = 1s / MIN_FREQ_HZ;
+  realtime last_tick = 0;
+  real current_freq;
+
+  always @(posedge clk_o) begin
+    this_cycle = $realtime - last_tick;
+    last_tick = $realtime;
+    current_freq = 1000ms / this_cycle;
+  end
+
+  task static get_frequency(input real target_freq);
+    real delta;
+    do begin
+      if (current_freq < target_freq) begin
+        delta = target_freq - current_freq;
+        if (voltage_ctrl_i != '1) voltage_ctrl_i = voltage_ctrl_i + 1;
+      end else if (current_freq > target_freq) begin
+        delta = current_freq - target_freq;
+        if (voltage_ctrl_i != '0) voltage_ctrl_i = voltage_ctrl_i - 1;
+      end else begin
+        delta = 0;
+      end
+      #10ps;
+    end while (delta > 100);
+    #1us;
+    $display("Reached target frequency: %0f (%0f) Hz with control voltage: %0d", current_freq,
+             target_freq, voltage_ctrl_i);
+  endtask
+
   initial begin
     $dumpfile("vco_tb.vcd");
     $dumpvars(0, vco_tb);
     fork
       begin
-        forever #1us $display("Time: %0tus", $realtime / 1us);
+        int i;
+        forever begin
+          #1us;
+          i++;
+          $display("Time: %0d us", i);
+        end
       end
     join_none
     voltage_ctrl_i <= 0;
-    #1us;
-    voltage_ctrl_i <= (2 ** RESOLUTION_BITS - 1) / 4;  // 25% of max frequency
-    #1us;
-    voltage_ctrl_i <= (2 ** RESOLUTION_BITS - 1) / 2;  // 50% of max frequency
-    #1us;
-    voltage_ctrl_i <= (3 * (2 ** RESOLUTION_BITS - 1)) / 4;  // 75% of max frequency
-    #1us;
-    voltage_ctrl_i <= (2 ** RESOLUTION_BITS - 1);  // Max frequency
-    #1us;
+    repeat (5) @(posedge clk_o);
+    get_frequency(200E3);
+    #100us;
     $finish;
   end
 
