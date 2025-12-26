20757b5430bf9ccd0b1beb96ee86ab1a79aa9746
Foez Ahmed <foez.official@gmail.com>
2025-12-27 01:21:09 +0600

    modified:   testbench/vco_tb.sv modified:   vco_tb_issue.gtkw

--- testbench/vco_tb.sv
+++ testbench/vco_tb.sv
@@ -29,30 +29,44 @@ module vco_tb;
   task static get_frequency(input real target_freq);
     real delta;
     int  delta_int;
+    bit  freq_incr;
+    bit  freq_decr;
     do begin
       if (current_freq < target_freq) begin
         delta = target_freq - current_freq;
         if (delta < 0) $display("Delta negative!");
         delta_int = delta;
-        if (voltage_ctrl_i != '1) voltage_ctrl_i <= voltage_ctrl_i + (1 + delta_int / 25);
+        if (voltage_ctrl_i != '1)
+          if ((voltage_ctrl_i + (1 + delta_int / 25)) > voltage_ctrl_i)
+            voltage_ctrl_i <= voltage_ctrl_i + (1 + delta_int / 25);
+          else voltage_ctrl_i <= '1;
+        freq_incr = 1;
+        freq_decr = 0;
       end else if (current_freq > target_freq) begin
         delta = current_freq - target_freq;
         if (delta < 0) $display("Delta negative!");
         delta_int = delta;
-        if (voltage_ctrl_i != '0) voltage_ctrl_i <= voltage_ctrl_i - (1 + delta_int / 25);
+        if (voltage_ctrl_i != '0)
+          if ((voltage_ctrl_i - (1 + delta_int / 25)) < voltage_ctrl_i)
+            voltage_ctrl_i <= voltage_ctrl_i - (1 + delta_int / 25);
+          else voltage_ctrl_i <= '0;
+        freq_decr = 1;
+        freq_incr = 0;
       end else begin
         delta = 0;
+        freq_incr = 0;
+        freq_decr = 0;
       end
       @(posedge clk_o);
-    end while ((delta*1000) > target_freq);
+    end while ((delta * 1000) > target_freq);
     $display("Reached target frequency: %0.2f (%0.2f) Hz with control voltage: %0d", current_freq,
              target_freq, voltage_ctrl_i);
     repeat (20) @(posedge clk_o);
   endtask
 
   initial begin
-    $dumpfile("vco_tb.vcd");
-    $dumpvars(0, vco_tb);
+    // $dumpfile("vco_tb.vcd");
+    // $dumpvars(0, vco_tb);
     fork
       begin
         int i;
@@ -70,7 +84,7 @@ module vco_tb;
     join_none
     voltage_ctrl_i <= 0;
     repeat (2) @(posedge clk_o);
-    repeat (100) get_frequency($urandom_range(32'd100_000, 32'd2_000_000_000));
+    repeat (1000) get_frequency($urandom_range(32'd100_000, 32'd2_000_000_000));
     $finish;
   end
 
