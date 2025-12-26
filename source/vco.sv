f15113cc1b8b43ade92303d84e56bfcd383d773c
Foez Ahmed <foez.official@gmail.com>
2025-12-26 13:12:26 +0600

    deleted:    source/pll.sv modified:   source/vco.sv deleted:    testbench/pll_tb.sv new file:   testbench/vco_tb.sv new file:   vco_tb.gtkw

--- source/vco.sv
+++ source/vco.sv
@@ -1,126 +1,36 @@
-// Write a markdown documentation for this systemverilog module:
-// Author : Foez Ahmed (foez.official@gmail.com)
-// This file is part of squared-studio : hardware
-// Copyright (c) 2025 squared-studio
-// Licensed under the MIT License
-// See LICENSE file in the repository root for full license information
-
-module vco (
-    input logic arst_ni,  // active low asynchronous reset
-    input logic freq_incr_i,  // increase frequency
-    input logic freq_decr_i,  // decrease frequency
-    input logic stable_cfg,  // configuration is stable
-
-    output logic clk_o,
-    output logic locked_o
+module vco #(
+    parameter int MIN_FREQ_HZ     = 100E3,  // 100 kHz
+    parameter int MAX_FREQ_HZ     = 10E9,   // 10 GHz
+    parameter int RESOLUTION_BITS = 32      // 32-bit resolution for frequency control word
+) (
+    input  logic [RESOLUTION_BITS-1:0] voltage_ctrl_i,  // Control voltage input for VCO
+    output logic                       clk_o
 );
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-LOCALPARAMS GENERATED
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+  realtime last_tick_time = 0.0;
+  realtime next_tick_time = 500ms / MIN_FREQ_HZ;
 
-  localparam realtime MIN_CLK_HALF_PERIOD = 50ps;  // Minimum allowed clock half period
-  localparam realtime MAX_CLK_HALF_PERIOD = 0.5ms;  // Maximum allowed clock half period
+  realtime freq_hz;
+  real factor;
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-SIGNALS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+  logic [RESOLUTION_BITS-1:0] max_voltage_ctrl = 2 ** RESOLUTION_BITS - 1;
 
-  realtime        clk_half_period;  // Variable to store the current clock half period
-  realtime        last_clk_tick;  // Variable to store the last clock tick time
-  bit             incr_ok;  // Flag to indicate if frequency increment is valid
-  bit             decr_ok;  // Flag to indicate if frequency decrement is valid
-  event           update;  // Event to trigger clock update
-  logic           locked_internal;  // Internal signal to indicate if the VCO is locked
-  logic    [15:0] locked_array;  // Array to track the locking status over multiple clock cycles
+  logic clk = 0;
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-ASSIGNMENTS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  assign locked_internal = arst_ni & decr_ok & incr_ok & stable_cfg; // VCO is locked if reset is de-asserted, increment and decrement are valid, and stable configuration is set.
-  assign locked_o = locked_array[15]; // Output the most significant bit of the locked array, indicating locked status.
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //-SEQUENTIALS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  // Frequency Increment Validation
-  always @(posedge freq_incr_i) begin
-    realtime time_record;
-    if (arst_ni) begin  // Only check when reset is de-asserted
-      time_record = $realtime;  // Record the current time
-      @(negedge freq_incr_i);  // Wait for the falling edge of the increment signal
-      time_record = $realtime - time_record;  // Calculate the pulse width of the increment signal
-      if (time_record < MIN_CLK_HALF_PERIOD) begin // Check if the pulse width is less than the minimum allowed half period
-        incr_ok = '1;  // If yes, set the increment flag to valid
-      end else begin
-        incr_ok = '0;  // Otherwise, set the increment flag to invalid
-      end
+  always #10ps begin
+    real v_in;
+    real v_max;
+    if ($realtime >= next_tick_time) begin
+      clk <= ~clk;
+      last_tick_time = $realtime;
     end
+    v_in = real'(unsigned'(voltage_ctrl_i));
+    v_max = real'(unsigned'(max_voltage_ctrl));
+    factor = v_in / v_max;
+    freq_hz = factor * (MAX_FREQ_HZ - MIN_FREQ_HZ) + MIN_FREQ_HZ;
+    next_tick_time = last_tick_time + (500ms / freq_hz);
   end
 
-  // Frequency Decrement Validation
-  always @(posedge freq_decr_i) begin
-    realtime time_record;
-    if (arst_ni) begin  // Only check when reset is de-asserted
-      time_record = $realtime;  // Record the current time
-      @(negedge freq_decr_i);  // Wait for the falling edge of the decrement signal
-      time_record = $realtime - time_record;  // Calculate the pulse width of the decrement signal
-      if (time_record < MIN_CLK_HALF_PERIOD) begin // Check if the pulse width is less than the minimum allowed half period
-        decr_ok = '1;  // If yes, set the decrement flag to valid
-      end else begin
-        decr_ok = '0;  // Otherwise, set the decrement flag to invalid
-      end
-    end
-  end
-
-  // Reset Trigger Event
-  always @(negedge arst_ni) begin
-    ->update;  // Trigger the update event when reset is asserted
-  end
-
-  // Periodic Trigger Event
-  always #25ps begin
-    ->update;
-  end
-
-  // Clock Generation Logic
-  always @(update) begin
-    if (~arst_ni) begin  // If reset is asserted
-      incr_ok = '0;  // Reset increment flag
-      decr_ok = '0;  // Reset decrement flag
-      clk_o <= '0;  // Reset clock output
-      last_clk_tick <= $realtime;  // Initialize last clock tick time
-      clk_half_period <= 1us;  // Initialize clock half period to 1us
-    end else begin  // If reset is de-asserted
-      if (freq_incr_i & ~freq_decr_i)
-        clk_half_period = clk_half_period - 1ps; // Decrement clock half period if only increment is active
-      if (freq_decr_i & ~freq_incr_i)
-        clk_half_period = clk_half_period + 1ps; // Increment clock half period if only decrement is active
-
-      if (clk_half_period < MIN_CLK_HALF_PERIOD)
-        clk_half_period = MIN_CLK_HALF_PERIOD;  // Limit clock half period to minimum value
-      if (clk_half_period > MAX_CLK_HALF_PERIOD)
-        clk_half_period = MAX_CLK_HALF_PERIOD;  // Limit clock half period to maximum value
-
-      if (($realtime) >= (last_clk_tick + clk_half_period)) begin // If current time is greater than or equal to the last clock tick plus half period
-        clk_o <= ~clk_o;  // Toggle the clock output
-        last_clk_tick <= $realtime;  // Update the last clock tick time
-      end
-    end
-  end
-
-  // Locking Logic
-  always_ff @(posedge clk_o or negedge locked_internal) begin
-    if (~locked_internal) begin  // If VCO is not locked
-      locked_array <= '0;  // Reset the locking array
-    end else begin  // If VCO is locked
-      locked_array <= {locked_array[14:0], 1'b1};  // Shift in a '1' to the locking array.
-      // This creates a history of the VCO being locked.
-      // The MSB of the array indicates if the VCO has been locked for a
-      // certain number of clock cycles, improving robustness.
-    end
-  end
+  assign clk_o = clk;
 
 endmodule
---
+++ testbench/vco_tb.sv
@@ -0,0 +1,40 @@
+module vco_tb;
+
+  parameter int MIN_FREQ_HZ = 100E3;
+  parameter int MAX_FREQ_HZ = 10E9;
+  parameter int RESOLUTION_BITS = 8;
+
+  logic [RESOLUTION_BITS-1:0] voltage_ctrl_i;
+  logic                       clk_o;
+
+  vco #(
+      .MIN_FREQ_HZ    (MIN_FREQ_HZ),
+      .MAX_FREQ_HZ    (MAX_FREQ_HZ),
+      .RESOLUTION_BITS(RESOLUTION_BITS)
+  ) u_vco (
+      .voltage_ctrl_i(voltage_ctrl_i),
+      .clk_o(clk_o)
+  );
+
+  initial begin
+    $dumpfile("vco_tb.vcd");
+    $dumpvars(0, vco_tb);
+    fork
+      begin
+        forever #1us $display("Time: %0tus", $realtime / 1us);
+      end
+    join_none
+    voltage_ctrl_i <= 0;
+    #1us;
+    voltage_ctrl_i <= (2 ** RESOLUTION_BITS - 1) / 4;  // 25% of max frequency
+    #1us;
+    voltage_ctrl_i <= (2 ** RESOLUTION_BITS - 1) / 2;  // 50% of max frequency
+    #1us;
+    voltage_ctrl_i <= (3 * (2 ** RESOLUTION_BITS - 1)) / 4;  // 75% of max frequency
+    #1us;
+    voltage_ctrl_i <= (2 ** RESOLUTION_BITS - 1);  // Max frequency
+    #1us;
+    $finish;
+  end
+
+endmodule
